350e3c6286099577a685cab6c7871e7eb058274c
Min1600 <aminhassan00@gmail.com>
2025-02-04 12:15:24 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {useState, useEffect } from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
@@ -242,15 +239,5 @@ const percentage = Math.round((((min*60) + sec)/durationInSeconds)*100)
   )
  }
 
+export { App }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
